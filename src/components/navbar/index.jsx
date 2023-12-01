ac55da161982b3cb0c2902917e0e97653d798f90
Andrew Shoener <a.b.shoener@gmail.com>
2023-12-01 17:41:38 -0600

    Add Link component to navbar Fix coloring

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import MenuIcon from "./MenuIcon.jsx";
 import LinksContainer from "./LinksContainer.jsx";
+import { Link } from "react-router-dom";
 
 export default () => {
   const [showLinks, setShowLinks] = useState(false);
@@ -8,7 +9,9 @@ export default () => {
   return (
     <header className="navbar">
       <div className="navbar-header-container">
-        <h1>Andrew Shoener</h1>
+        <Link to="/">
+          <h1>Andrew Shoener</h1>
+        </Link>
         <MenuIcon action={() => setShowLinks(!showLinks)} />
       </div>
       <LinksContainer showLinks={showLinks}></LinksContainer>
