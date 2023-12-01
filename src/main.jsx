c33b1ff770a2e55b2df8ff5f7e00e0acf94f5d6b
Andrew Shoener <a.b.shoener@gmail.com>
2023-12-01 17:14:18 -0600

    Add css from original portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./reset.css";
+import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
