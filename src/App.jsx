50711c72cea3a027b811aee31389eee24b697905
Andrew Shoener <a.b.shoener@gmail.com>
2023-12-01 17:38:23 -0600

    Remove unused count state variable

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./components/navbar";
 
 function App(props) {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Navbar />
