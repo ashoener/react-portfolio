71470b6227ab2bd468d4cfb49ee9ef9c4d7a5033
Andrew Shoener <a.b.shoener@gmail.com>
2023-12-03 11:31:08 -0600

    Server all not found files as index.html

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 
 app.use(express.static(__dirname + "/dist"));
 
+app.get("*", (req, res) => res.sendFile(__dirname + "/dist/index.html"));
+
 app.listen(PORT, "127.0.0.1", () => {
   console.log(`Server is running on port ${PORT}`);
 });
